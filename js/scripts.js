c91edb0fe2b20ba5376eb254c8d1b98286508403
Erich Richter <erich.richter@gmail.com>
2020-01-17 13:49:28 -0800

    add radio button style selection and functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,65 +1,50 @@
-$(document).ready(function() {
-  $(".btn").click(function() {
-    var question1 = ($("input#1stQuestion").val());
-    var question2 = ($("input#2ndQuestion").val());
-    var question3 = ($("input#3rdQuestion").val());
+$(document).ready(function(){
   
-  if (question1 === "yes"){
-    question1 = 1;
-  }else{
-    question1=0;
-  }
+  var result = 0;
   
-  if (question2 === "yes"){
-    question2 = 1;
-  }else{
-    question2 = 0;
-  }
-  
-  if (question3 === "yes"){
-    question3 = 1;  
-  }else{
-    question3 = 0;
-  }
-  
-  if (question1 + question2 + question3 < 2){
+  $("input[type='button']").click(function(){
+      var radioValue = parseInt($("input[name='cars']:checked").val());
+      if(radioValue){
+          result += 1;
+      }
+          var radioValue = parseInt($("input[name='trucks']:checked").val());
+      if(radioValue){
+          result += 1;
+        
+      }
+ 
+      var radioValue = parseInt($("input[name='dogs']:checked").val());
+      if(radioValue){
+          result += 1;
+        
+      }
 
-  
-    $("#output").text("your language is python");
-    $("#output").show();
-  }
-  
-  if (question1 + question2 + question3 == 3){
+      var radioValue = parseInt($("input[name='cats']:checked").val());
+      if(radioValue){
+          result += 1;
+        
+      }
+      //alert(result);
+      
+      if (result === 0){
+        $("#output").text("please select some items");
+        $("#output").show();
 
-  
-    $("#output").text("your language is C#");
-    $("#output").show();
-  }
-  //if (side1 && side2 && side3) {  
-    //if (side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2) {
-      //$("#output").text("Not a triangle, sorry");
-      //$("#output").show();
-    //} else if (side1 === side2 && side1 === side3) {
-      //$("#output").text("Equilateral");
-      //$("#output").show();
-   // } else if (side1 === side2 && side1 !== side3 || side1 === side3 && side2 !== side1 || side2 //=== side3 && side2 !== side1) {
-      //$("#output").text("Isosceles");
-      //$("#output").show();
-    //} else if (side1 !== side2 && side1 !== side3 && side2 !== side3) {
-      //$("#output").text("Scalene");
-      //$("#output").show();
-    //} else {
-      //$("#output").text("I don't know what to tell you");
-      //$("#output").show();
-    //}
-  //} else {
-    //$("#output").text("Please try again and enter numbers");
-    //$("#output").show();
-  //}
+      }
+      
+      if (result === 4){
+        $("#output").text("your language is python");
+        $("#output").show();
 
+      }
 
+      if (result === 6){
+        $("#output").text("your language is c#");
+        $("#output").show();
 
+      }
 
+    
 
 event.preventDefault();
   });
