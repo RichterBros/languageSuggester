fe48b3ba273119db0d8bcc83af4c20ac4750278e
Erich Richter <erich.richter@gmail.com>
2020-01-27 21:31:34 -0800

    clean up code

--- js/scripts.js
+++ js/scripts.js
@@ -3,54 +3,33 @@ $(document).ready(function(){
   var result = 0;
   var strList = []
   var z=0
-  //var radioValue2= document.getElementsByName('rbtn');
+  
   $("input[type='button']").click(function(){
     event.preventDefault();
     
-  
-      var arr = document.getElementById('rbtn').getElementsByTagName('input')
-    
+    var arr = document.getElementById('rbtn').getElementsByTagName('input')
+      
     for (i =0; i<arr.length; i++){
       if (arr[i].checked===true){
-        result +=1;
+      result +=1;
       }
     }
-    
-    
-    
-    console.log (arr[0].checked)
-    
       
-
-
-      
-      if (result === 0){
-        $("#output").text("Please select some items.");
-        $("#output").show();
-
-      }
-      
-      if (result >= 1 && result <= 3){
-        $("#output").text("Your language is python!");
-        $("#output").show();
-
-      }
-
-      if (result >= 4 && result <= 7){
-        $("#output").text("Your language is javascript!");
-        $("#output").show();
-
-      }
-
-      if (result >= 6){
-        $("#output").text("Your language is c#!");
-        $("#output").show();
-
-      }
-
-      console.log(arr);
-
-   
- 
-    });
+    if (result === 0){
+      $("#output").text("Please select some items.");
+      $("#output").show();
+    }
+    if (result >= 1 && result <= 3){
+      $("#output").text("Your language is python!");
+      $("#output").show();
+    }
+    if (result >= 4 && result <= 7){
+      $("#output").text("Your language is javascript!");
+      $("#output").show();
+    }
+    if (result >= 6){
+      $("#output").text("Your language is c#!");
+      $("#output").show();
+    }
+  });
 });
