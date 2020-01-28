6c0a48125def9cf8725562b1047a42ae946c94a2
Erich Richter <erich.richter@gmail.com>
2020-01-27 21:19:59 -0800

    add looping functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,61 +1,56 @@
 $(document).ready(function(){
-  
+  var test = 0;
   var result = 0;
-  
+  var strList = []
+  var z=0
+  //var radioValue2= document.getElementsByName('rbtn');
   $("input[type='button']").click(function(){
+    event.preventDefault();
+    
   
-  var radioValue = parseInt($("input[name='cars']:checked").val());
-  if(radioValue){
-      result += 1;
-  }
-  var radioValue = parseInt($("input[name='trucks']:checked").val());
-  else if(radioValue){
-      result += 1;
-  }
-  var radioValue = parseInt($("input[name='dogs']:checked").val());
-  else if(radioValue){
-      result += 1;
-  }
-  var radioValue = parseInt($("input[name='cats']:checked").val());
-  else if(radioValue){
-      result += 1;
-  }
-  var radioValue = parseInt($("input[name='pie']:checked").val());
-  else if(radioValue){
-      result += 1;
-  }
-  var radioValue = parseInt($("input[name='pizza']:checked").val());
-  else if(radioValue){
-      result += 1;
-  }
-  var radioValue = parseInt($("input[name='coffee']:checked").val());
-  else if(radioValue){
-      result += 1;
-  }
-  var radioValue = parseInt($("input[name='exercize']:checked").val());
-  if(radioValue){
-      result += 1;
-  }
-  
-  if (result === 0){
-  $("#output").text("Please select some items.");
-  $("#output").show();
-  }
-  else if (result >= 1 && result <= 3){
-  $("#output").text("Your language is python!");
-  $("#output").show();
-  }
-  else if (result >= 4 && result <= 7){
-  $("#output").text("Your language is javascript!");
-  $("#output").show();
-  }
-  else if (result >= 6){
-  $("#output").text("Your language is c#!");
-  $("#output").show();
-  }
+      var arr = document.getElementById('rbtn').getElementsByTagName('input')
+    
+    for (i =0; i<arr.length; i++){
+      if (arr[i].checked===true){
+        result +=1;
+      }
+    }
+    
+    
+    
+    console.log (arr[0].checked)
+    
+      
 
-     
 
-  event.preventDefault();
-  });
+      
+      if (result === 0){
+        $("#output").text("Please select some items.");
+        $("#output").show();
+
+      }
+      
+      if (result >= 1 && result <= 3){
+        $("#output").text("Your language is python!");
+        $("#output").show();
+
+      }
+
+      if (result >= 4 && result <= 7){
+        $("#output").text("Your language is javascript!");
+        $("#output").show();
+
+      }
+
+      if (result >= 6){
+        $("#output").text("Your language is c#!");
+        $("#output").show();
+
+      }
+
+      console.log(arr);
+
+   
+ 
+    });
 });
